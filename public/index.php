dc23923812449f73777fe4d13657b5f871dccd6d
Gitromanus <netesn@gmail.com>
2026-09-06 17:36:49 +0500

    Restore full UI from text parts 0-5

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
 $cache = __DIR__ . '/index.full.php';
 if (!is_file($cache) || filesize($cache) < 1000) {
-    $b = file_get_contents(__DIR__ . '/index.gz_a.b64') . file_get_contents(__DIR__ . '/index.gz_b.b64');
-    file_put_contents($cache, gzdecode(base64_decode($b)));
+    $t = '';
+    for ($i = 0; $i < 6; $i++) {
+        $t .= file_get_contents(__DIR__ . "/part_$i.txt");
+    }
+    file_put_contents($cache, $t);
 }
 require $cache;
