2cfe4d840b7abe8b2d928d3a6efd8e1f07716fff
Gitromanus <netesn@gmail.com>
2026-09-15 06:52:27 +0500

    FIX: require UI files so PHP tabs/data execute (not echo file_get_contents)

--- public/admin/admin_app.full.php
+++ public/admin/admin_app.full.php
@@ -1,4 +1,4 @@
 <?php
 require __DIR__ . '/admin_logic.php';
-echo file_get_contents(__DIR__ . '/admin_ui_a.php');
-echo file_get_contents(__DIR__ . '/admin_ui_b.php');
+require __DIR__ . '/admin_ui_a.php';
+require __DIR__ . '/admin_ui_b.php';
